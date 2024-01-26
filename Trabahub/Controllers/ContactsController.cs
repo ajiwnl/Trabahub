f0d42349451f9b505156002a33aa1d591afeca45
ajiwnl <alvinjohnaranetz@gmail.com>
2024-01-27 07:06:18 +0800

    Initial Contacts Design

---
+++ Trabahub/Controllers/ContactsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trabahub.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["ActivePage"] = "Contact Us";
+            return View();
+        }
+    }
+}
