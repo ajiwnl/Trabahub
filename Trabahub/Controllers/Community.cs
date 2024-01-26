c3a02316bb6f7ef639d8931f0665f8277ecc9fd0
ajiwnl <alvinjohnaranetz@gmail.com>
2024-01-27 07:24:57 +0800

    Contacts First Part Design

---
+++ Trabahub/Controllers/Community.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trabahub.Controllers
+{
+    public class Community : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
