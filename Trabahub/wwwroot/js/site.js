2c11f757a85d7144c4833df298dfbb87cd1210e4
Soliken <kennethrex456@gmail.com>
2024-01-26 20:44:07 +0800

    Commit Spaces

--- Trabahub/wwwroot/js/site.js
+++ Trabahub/wwwroot/js/site.js
@@ -5,12 +5,10 @@ function handleKeyPress(event) {
     }
 }
 
-async function MapSearch() {
+function MapSearch() {
     var GetMapInput = document.getElementById('textmap').value;
-    var GetMapSRC = document.getElementById('map');
+    var GetMapSRC = document.getElementById('map').src;
 
     var createInputQuery = 'https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=(' + GetMapInput + ')&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;&output=embed';
     GetMapSRC.src = createInputQuery;
-
-    console.log(createInputQuery);
 }
