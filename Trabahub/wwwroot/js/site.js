fb7c9291d4468904e96502ae5fefbec9e7a4d5bc
Soliken <kennethrex456@gmail.com>
2024-01-26 19:30:09 +0800

    commit staged index map and js

--- Trabahub/wwwroot/js/site.js
+++ Trabahub/wwwroot/js/site.js
@@ -5,9 +5,9 @@ function handleKeyPress(event) {
     }
 }
 
-function MapSearch() {
+async function MapSearch() {
     var GetMapInput = document.getElementById('textmap').value;
-    var GetMapSRC = document.getElementById('map').src;
+    var GetMapSRC = document.getElementById('map');
 
     var createInputQuery = 'https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=(' + GetMapInput + ')&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;&output=embed';
     GetMapSRC.src = createInputQuery;
