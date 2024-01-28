a000bbc47078909289029e21ac48650bdd7c7cfe
Soliken <kennethrex456@gmail.com>
2024-01-28 23:26:33 +0800

    Credentials (Only Login) and Changes

---
+++ Trabahub/Controllers/CredentialsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trabahub.Controllers
+{
+    public class CredentialsController : Controller
+    {
+        //GET
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+        //POST USER
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        //POST OWNERS
+        public IActionResult RegisterOwners()
+        {
+            return View();
+        }
+    }
+}
--- Trabahub/wwwroot/js/site.js
+++ Trabahub/wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿
+﻿let isNotVisible = false;
+function NavVision(VisionState) {
+    var navbar = document.getElementById('layoutnav');
+    var footer = document.getElementById('layoutfoot');
+
+    
+}
+document.addEventListener('DOMContentLoaded', function () {
+    var navbar = document.getElementById("layoutnav");
+    var footer = document.getElementById('layoutfoot');
+
+    var visionState = localStorage.getItem('VisionState');
+
+    
+});
+
