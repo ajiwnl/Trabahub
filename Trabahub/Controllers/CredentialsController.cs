fa3aa70be420df178e2eb4a3efa85370eaa205f2
ajiwnl <alvinjohnaranetz@gmail.com>
2024-02-05 23:02:41 +0800

    Forgot Password Working

--- Trabahub/Controllers/CredentialsController.cs
+++ Trabahub/Controllers/CredentialsController.cs
@@ -30,7 +30,22 @@ namespace Trabahub.Controllers
         public IActionResult Login(string emaillog, string passwordlog)
         {
 			var existingEmail = _context.Credentials.FirstOrDefault(x => x.Email == emaillog);
-			if(existingEmail == null)
+            var forgotMessage = TempData["ForgotMessage"];
+
+            if (forgotMessage != null)
+            {
+                TempData.Remove("ForgotMessage");
+
+                ViewBag.ForgotMessage = forgotMessage;
+
+                // Check if SweetAlert flag is set
+                if (TempData.Peek("ShowSweetAlert") is bool showSweetAlert && showSweetAlert)
+                {
+                    ViewBag.ShowSweetAlert = true;
+                    TempData.Remove("ShowSweetAlert"); // Remove the flag to prevent showing again
+                }
+            }
+            if (existingEmail == null)
             {
                 TempData["UserNull"] = "Email Not Found";
                 return View("Login");
@@ -80,11 +95,33 @@ namespace Trabahub.Controllers
             return View("Login");
         }
 
-        //EMAIL PASSWORD
-        public IActionResult ForgotPassword()
+		[HttpGet]
+		public IActionResult ForgotPassword(string? code)
+		{
+			var useremail = _context.Credentials.Where(s => s.Email == code).FirstOrDefault();
+
+			return View(useremail);
+		}
+
+
+
+        [HttpPost]
+        public IActionResult ForgotPassword(Credentials credentialsEdit)
         {
-            return View();
+            var credential = _context.Credentials.FirstOrDefault(s => s.Email == credentialsEdit.Email);
+
+            if (credential != null)
+            {
+                // Update the existing credential with the new password
+                credential.Password = credentialsEdit.Password;
+                _context.SaveChanges();
+            }
+
+            TempData["ForgotMessage"] = "Password Updated Successfully!";
+            TempData["ShowSweetAlert"] = true;
+            return View(credentialsEdit);
         }
+
 
         public void SaveEntry(Credentials addCredentialEntry)
         {
@@ -97,6 +134,13 @@ namespace Trabahub.Controllers
 
             _context.Credentials.Add(credentials);
             _context.SaveChanges();
+        }
+
+        [HttpPost]
+        public JsonResult CheckEmail(string email)
+        {
+            bool emailExists = _context.Credentials.Any(s => s.Email == email);
+            return Json(new { exists = emailExists });
         }
     }
 }
