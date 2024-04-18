361162c2f95df067c1aa7ebfa2873b9178ac95c8
Alvin John Araneta <alvinjohnaranetz@gmail.com>
2024-04-18 09:57:25 +0800

    Total Listing view for specific owner working

--- Trabahub/Controllers/HomeController.cs
+++ Trabahub/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Trabahub.Models;
 using Trabahub.Helpers;
 using Stripe;
 using System.IO.Pipelines;
+using Newtonsoft.Json;
 
 namespace Trabahub.Controllers
 {
@@ -23,41 +24,52 @@ namespace Trabahub.Controllers
 			_context = context;
 		}
 
-        public async Task<IActionResult> IndexAsync()
+        public IActionResult Index()
         {
+            // Get the logged-in user's type
+            var userType = HttpContext.Session.GetString("UserType");
+
+            // Check if the user is an owner
+            if (userType == "Owner")
+            {
+                // Get the logged-in owner's username
+                var ownerUsername = HttpContext.Session.GetString("Username");
+
+                // Query the listings belonging to the logged-in owner
+                var getTotalListings = _context.Listing.Count(l => l.OwnerUsername == ownerUsername);
+
+                // Set the session for TotalListing
+                HttpContext.Session.SetString("TotalListing", getTotalListings.ToString());
+            }
+            else
+            {
+                // For non-owners, set TotalListing to 0
+                HttpContext.Session.SetString("TotalListing", "0");
+            }
+
+            // Retrieve other counts and data as before
             var getTotalUsers = _context.Credentials.Count();
-            var getTotalListings = _context.Listing.Count();
             var getTotalInteractions = _context.ListInteraction.Count();
-		var userType = HttpContext.Session.GetString("UserType");
 
             var getTotalPrice = _context.Analytics.FirstOrDefault();
             if (getTotalPrice != null)
             {
                 double priceDouble = Convert.ToDouble(getTotalPrice.TotalIncome);
                 HttpContext.Session.SetString("TotalCharges", "₱" + priceDouble.ToString());
-            } else
-            {
-                HttpContext.Session.SetString("TotalCharges", "₱0");
-
-            }
-
-            // Access the CalculateTotalChargesFromStripe method
-            var getTotalCharges = await StripeHelper.CalculateTotalChargesFromStripe();
-
-            HttpContext.Session.SetString("TotalListing", getTotalListings.ToString());
-            HttpContext.Session.SetString("TotalUsers", getTotalUsers.ToString());
-            HttpContext.Session.SetString("TotalBooks", getTotalInteractions.ToString());
-
-            if (userType == "Owner")
-            {
-              
-                return View();
             }
             else
             {
-
-                return View();
+                HttpContext.Session.SetString("TotalCharges", "₱0");
             }
+
+            // Access the CalculateTotalChargesFromStripe method synchronously
+            var getTotalCharges = StripeHelper.CalculateTotalChargesFromStripe().Result;
+
+            HttpContext.Session.SetString("TotalUsers", getTotalUsers.ToString());
+            HttpContext.Session.SetString("TotalBooks", getTotalInteractions.ToString());
+
+            // Return the appropriate view based on user type
+            return View();
         }
 
         public IActionResult Privacy()
